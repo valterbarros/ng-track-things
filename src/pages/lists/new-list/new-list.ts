67c26112d66b11fea13a47d3ab03c2a174c152b2
valterbarros <valterinside@gmail.com>
2019-10-11 22:52:17 -0300

    master
    - Add normalize and start create sublist form
    
    Signed-off-by: valterbarros <valterinside@gmail.com>

--- src/pages/lists/new-list/new-list.ts
+++ src/pages/lists/new-list/new-list.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { FormBuilder, Validators, FormGroup, FormArray } from '@angular/forms';
 import { firebaseDb } from '../../../providers/firebase'
 
 @Component({
@@ -9,12 +9,32 @@ import { firebaseDb } from '../../../providers/firebase'
 })
 export class NewListPage implements OnInit {
   listForm: FormGroup;
+  subLists: FormArray;
 
   constructor (private formBuilder: FormBuilder) {
     this.listForm = formBuilder.group({
-      name: '',
-      description: ''
-    })
+      title: '',
+      description: '',
+      subLists: this.formBuilder.array([this.createSubList()])
+    });
+  }
+
+  removeSubList(index) {
+    const subLists = this.listForm.get('subLists') as FormArray;
+
+    subLists.removeAt(index);
+  }
+
+  addSubList () {
+    this.subLists = this.listForm.get('subLists') as FormArray;
+
+    this.subLists.push(this.createSubList());
+  }
+
+  createSubList(): FormGroup {
+    return this.formBuilder.group({
+      title: '',
+    });
   }
 
   ngOnInit() {
@@ -22,7 +42,30 @@ export class NewListPage implements OnInit {
   }
 
   onSubmit () {
-    console.log('ah não')
-    console.log(this.listForm.value)
+    const listParams = {
+      title: this.listForm.name,
+      repeat_days: this.repeatDays,
+      description: this.description,
+      deadline: this.deadline
+    }
+
+    const batch = firebaseDb.batch()
+    const listRef = firebaseDb.collection('lists').doc()
+
+    for (let subList of ruleForm.sub_lists) {
+      let title = subList.title
+      let listId = listRef.id
+      let subListRef = firebaseDb.collection('sub_lists').doc()
+
+      batch.set(subListRef, { title, list_id: listId })
+    }
+
+    batch.set(listRef, listParams)
+
+    batch.commit().then(() => {
+      return this.$router.push({name: 'Lists'})
+    }).catch(function (error) {
+      console.error('Error during batch commit: ', error)
+    })
   }
 }
